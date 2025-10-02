a903c4445f9b7f2b361bb79f7313f0106295e18f
Tim Fletcher <timfletcher88@gmail.com>
2025-10-02 16:52:36 +1000

    Added circuit-breaker for rare infinite loops

--- modules/gatsby_endpoints/src/GatsbyEndpointGenerator.php
+++ modules/gatsby_endpoints/src/GatsbyEndpointGenerator.php
@@ -193,6 +193,22 @@ class GatsbyEndpointGenerator {
           // Continue building out the JSON:API path to this related field.
           if ($current_field) {
             $field_name = $current_field . '.' . $field_name;
+            
+            /**
+             * Parks Australia updates
+             * 
+             * tags: gatsby-custom, infinite loop, memory leak
+             *
+             * Checks that the first part of the field name is not repeated,
+             * indicating the start of an infinite loop e.g a node containing
+             * an ER field pointing to nodes of the same type, which point to 
+             * more nodes of the same type etc
+             */
+            
+            $parts = explode('.', $field_name);
+            if (count($parts) > 1 && $parts[0] === $parts[1]) {
+              continue;
+            }
           }
           $params['include'][] = $field_name;
 
